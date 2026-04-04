13075a7be41246a2a3ea17b4b15cac452a58f21e
AnasImloul <anas.imloul27@gmail.com>
2026-04-04 19:14:00 +0100

    infra: add rate limiting

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,3 +9,23 @@ resource "cloudflare_workers_route" "root_domain_route" {
   pattern = "${var.zone_name}/*"
   script  = var.router_worker_name
 }
+
+resource "cloudflare_ruleset" "global_rate_limit_all_pages" {
+  zone_id = data.cloudflare_zone.root.zone_id
+  name    = "Global rate limit - all pages"
+  kind    = "zone"
+  phase   = "http_ratelimit"
+
+  rules = [{
+    ref         = "rl_all_pages"
+    description = "Protect Worker daily quota from abuse"
+    expression  = "http.host eq \"${var.zone_name}\""
+    action      = "managed_challenge"
+    ratelimit = {
+      characteristics     = ["ip.src", "cf.colo.id"]
+      period              = 60
+      requests_per_period = 300
+      mitigation_timeout  = 600
+    }
+  }]
+}
